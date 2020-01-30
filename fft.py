9fc14c741b92f417a3ca0c83b36cf1e2d677f42f
Oli Thompson <olithompson@rocketmail.com>
2020-01-30 23:01:40 +0000

    add save plot

--- fft.py
+++ fft.py
@@ -11,4 +11,4 @@ b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [
 c = fft(b) # calculate fourier transform (complex numbers list)
 d = len(c)/2  # you only need half of the fft list (real signal symmetry)
 plt.plot(abs(c[:(d-1)]),'r') 
-plt.show()
+plt.savefig(str(sys.argv[1])+' fft.png')
