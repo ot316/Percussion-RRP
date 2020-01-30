8ebaf62a3ffb4ebdf0935f31074c7e4181286b0e
Oli Thompson <olithompson@rocketmail.com>
2020-01-30 22:46:48 +0000

    add fft arg

--- fft.py
+++ fft.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+import sys
 from scipy.fftpack import fft
 from scipy.io import wavfile # get the api
-fs, data = wavfile.read('test.wav') # load the data
+try:
+    fs, data = wavfile.read(str(sys.argv[1])) # load the data
+except: 
+    print("specify filename as argument")
 a = data.T[0] # this is a two channel soundtrack, I get the first track
 b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
 c = fft(b) # calculate fourier transform (complex numbers list)
