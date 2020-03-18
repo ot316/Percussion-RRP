3163b44cddf8c564f163ced1558fd07f84d60f86
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2020-03-18 08:56:51 +0000

    Add files via upload

--- SignalAnalysis-MATLAB/RRP_DataAnalysis_v6_freq.m
+++ SignalAnalysis-MATLAB/RRP_DataAnalysis_v6_freq.m
@@ -209,25 +209,28 @@ xlabel('Sample(time)');
 ylabel('Amplitude');
 title('S-Golay Filtering B');
 
-% % SGolay O-7, FL-13
-% subplot(2,1,2)
-% SG_order = 7;
-% SG_framelen = 13;
-% sgf_A = sgolayfilt(A{i},SG_order,SG_framelen);
-% 
-% plot(t_A{i},A{i})
-% hold on
-% plot(t_A{i},sgf_A)
-% 
-% legend('signal','sgolay order 7, fl 13')
-% xlabel('Sample(time)');
-% ylabel('Amplitude')
-% title('S-Golay Filtering')
+%% Till heree
 
-% It seems like S-Golay is not able to smooth the signal as much as we'd
-% like. This is expected as it is more effective at preserving high frequency 
-% signal components but less successful at rejecting noise. This is OK.
+% SGolay O-7, FL-13
+subplot(2,1,2)
+SG_order = 7;
+SG_framelen = 13;
+sgf_A = sgolayfilt(A{i},SG_order,SG_framelen);
 
+plot(t_A{i},A{i})
+hold on
+plot(t_A{i},sgf_A)
+
+legend('signal','sgolay order 7, fl 13')
+xlabel('Sample(time)');
+ylabel('Amplitude')
+title('S-Golay Filtering')
+
+It seems like S-Golay is not able to smooth the signal as much as we'd
+like. This is expected as it is more effective at preserving high frequency 
+signal components but less successful at rejecting noise. This is OK.
+
+% From here
 %% ======================================== Spectogram ========================================
 % figure(5);
 % for m = 45
@@ -243,6 +246,7 @@ m=23;
 %frequency levels) using DB10 wavelet. The x-axis is time, and y-axis is
 %frequency. Higher the scale, lower the frequency. The color map is the
 %intensity of a particular frequency at a given time.
+
 CWTcoeffs_A = cwt(sgf_A{m},5:5:500,'sym10','plot');     %cwt for continuous 1D wavelet transform
 colormap jet; 
 colorbar;
@@ -253,8 +257,6 @@ ylabel('Scale level (Inverse of frequency)','fontsize',16)
 
 % ------------------------------------------------------- B
 figure(7);
-for m = 1:
-chopB = sgf_B{m};
 CWTcoeffs_B = cwt(sgf_B{m},5:5:500,'sym10','plot');     %cwt for continuous 1D wavelet transform
 colormap jet; 
 colorbar;
@@ -263,25 +265,145 @@ title('Matrix B - Far Nodule')
 xlabel('Sample (time)','fontsize',16);
 ylabel('Scale level (Inverse of frequency)','fontsize',16)
 
-%Same in 3D
+% ============================== CWT Coefficients - Analysis ===============================
 % ------------------------------------------------------- A
+%Continuous wavelet transform up to 500 scale levels (analogous to
+%frequency levels) using DB10 wavelet. The x-axis is time, and y-axis is
+%frequency. Higher the scale, lower the frequency. The color map is the
+%intensity of a particular frequency at a given time.
+
+fun = @(x) x(1:1000,:)  % Function to be repeated at each cell, extract first 1000 values
+
+for i = 1:A_col
+    %sgf_A_trimmed{i} = cellfun(fun,sgf_A)
+    sgf_A_trimcurr = sgf_A{i};
+    
+    sgf_A_trim(1,:) = {sgf_A_trimcurr(1:1000,:)};
+    sgf_A_trim(2,:) = {sgf_A_trimcurr(1:25-1,:)};
+    sgf_A_trim(3,:) = {sgf_A_trimcurr(25:50-1,:)};
+    sgf_A_trim(4,:) = {sgf_A_trimcurr(50:75-1,:)};
+    sgf_A_trim(5,:) = {sgf_A_trimcurr(75:100-1,:)};
+    sgf_A_trim(6,:) = {sgf_A_trimcurr(100:125-1,:)};
+    sgf_A_trim(7,:) = {sgf_A_trimcurr(125:150-1,:)};
+    sgf_A_trim(8,:) = {sgf_A_trimcurr(150:175-1,:)};
+    sgf_A_trim(9,:) = {sgf_A_trimcurr(175:200-1,:)};
+    sgf_A_trim(10,:) = {sgf_A_trimcurr(200:225-1,:)};
+    sgf_A_trim(11,:) = {sgf_A_trimcurr(225:250-1,:)};
+    sgf_A_trim(12,:) = {sgf_A_trimcurr(250:275-1,:)};
+    sgf_A_trim(13,:) = {sgf_A_trimcurr(275:300-1,:)};
+    sgf_A_trim(14,:) = {sgf_A_trimcurr(300:325-1,:)};
+    sgf_A_trim(15,:) = {sgf_A_trimcurr(325:350-1,:)};
+    sgf_A_trim(16,:) = {sgf_A_trimcurr(350:375-1,:)};
+    sgf_A_trim(17,:) = {sgf_A_trimcurr(375:400-1,:)};
+    sgf_A_trim(18,:) = {sgf_A_trimcurr(400:425-1,:)};
+    sgf_A_trim(19,:) = {sgf_A_trimcurr(425:450-1,:)};
+    sgf_A_trim(20,:) = {sgf_A_trimcurr(450:475-1,:)};
+    sgf_A_trim(21,:) = {sgf_A_trimcurr(475:500-1,:)};
+    % -------------- 500
+    sgf_A_trim(22,:) = {sgf_A_trimcurr(500:525-1,:)};
+    sgf_A_trim(23,:) = {sgf_A_trimcurr(525:550-1,:)};
+    sgf_A_trim(24,:) = {sgf_A_trimcurr(550:575-1,:)};
+    sgf_A_trim(25,:) = {sgf_A_trimcurr(575:600-1,:)};
+    sgf_A_trim(26,:) = {sgf_A_trimcurr(600:625-1,:)};
+    sgf_A_trim(27,:) = {sgf_A_trimcurr(625:650-1,:)};
+    sgf_A_trim(28,:) = {sgf_A_trimcurr(650:675-1,:)};
+    sgf_A_trim(29,:) = {sgf_A_trimcurr(675:700-1,:)};
+    sgf_A_trim(30,:) = {sgf_A_trimcurr(700:725-1,:)};
+    sgf_A_trim(31,:) = {sgf_A_trimcurr(725:750-1,:)};
+    sgf_A_trim(32,:) = {sgf_A_trimcurr(750:775-1,:)};
+    sgf_A_trim(33,:) = {sgf_A_trimcurr(775:800-1,:)};
+    sgf_A_trim(34,:) = {sgf_A_trimcurr(800:825-1,:)};
+    sgf_A_trim(35,:) = {sgf_A_trimcurr(825:850-1,:)};
+    sgf_A_trim(36,:) = {sgf_A_trimcurr(850:875-1,:)};
+    sgf_A_trim(37,:) = {sgf_A_trimcurr(875:900-1,:)};
+    sgf_A_trim(38,:) = {sgf_A_trimcurr(900:925-1,:)};
+    sgf_A_trim(39,:) = {sgf_A_trimcurr(925:950-1,:)};
+    sgf_A_trim(40,:) = {sgf_A_trimcurr(950:975-1,:)};
+    sgf_A_trim(41,:) = {sgf_A_trimcurr(975:1000-1,:)};
+end
+
+%% Method 1 : Show all, difficult method
+% figure(8);
+% 
+% for i = 1:A_col
+%      for k = 1:41
+%         CWTcoeffs_A_Complete{k,i} = cwt(sgf_A_trim{k,i},5:5:500,'sym10','plot');     %cwt for continuous 1D wavelet transform
+%         sgf_A_trim{k,i}
+%      end
+% end
+%
+% Real timestep, better if we avg tho
+% % for i = 1:A_col
+% %      for k = 1:3
+% %         real_timestep_curr = CWTcoeffs_A_Complete{k,i}
+% % %         real_timestep{k,i} = real_timestep_curr
+% %      end
+% % end
+
+%% Method 2 : Avg all
 figure(8);
-surf(CWTcoeffs_A); 
-colormap jet;
+
+% Note that:
+% CWTcoeffs_A_Complete{A,B} = (C, D)
+    % A = number of time steps (41)
+    % B = audio samples (48)
+    % C = Inverse Frequency Steps (100)
+    % D = trimmed time steps (25)
+% We wanna average for all B
+
+% ----------------------------
+% CWTcoeffs_A_avg{k} = mean(sgf_A_trim, 2);     % Mean
+% X = cat(1,sgf_A_trim{:})
+% mean(X,1)
+
+% for i = 1:A_col
+%      for k = 1:41
+%         %M = mean(A,dim) returns the mean along dimension dim. 
+%         %For example, if A is a matrix, then mean(A,2) is a column vector containing the mean of each row.
+%         CWTcoeffs_A_avg{k} = mean(sgf_A_trim, 2);     % Mean
+%      end
+% end
+% ----------------------------
+% PROBLEM HERE!
+% ----------------------------
+
+for i = 1:A_col
+     for k = 1:41
+%         M = mean(A,dim) returns the mean along dimension dim. 
+%         For example, if A is a matrix, then mean(A,2) is a column vector containing the mean of each row.
+        CWTcoeffs_A_avg{k} = mean(sgf_A_trim, 2);     % Mean
+     end
+end
+
+plot(CWTcoeffs_A_Complete{:} ,smth,'r');
+colormap jet; 
 colorbar;
 
-shading('interp'); view(-30,10);
+title('Matrix A - Close Nodule')
 xlabel('Sample (time)','fontsize',16);
 ylabel('Scale level (Inverse of frequency)','fontsize',16)
-zlabel('Intensity (coefficients)')
 
-% ------------------------------------------------------- B
-figure();
-surf(CWTcoeffs_B); 
-colormap jet;
-colorbar;
 
-shading('interp'); view(-30,10);
-xlabel('Sample (time)','fontsize',16);
-ylabel('Scale level (Inverse of frequency)','fontsize',16)
-zlabel('Intensity (coefficients)')
+
+% %Same in 3D
+% % ------------------------------------------------------- A
+% figure(8);
+% surf(CWTcoeffs_A); 
+% colormap jet;
+% colorbar;
+% 
+% shading('interp'); view(-30,10);
+% xlabel('Sample (time)','fontsize',16);
+% ylabel('Scale level (Inverse of frequency)','fontsize',16)
+% zlabel('Intensity (coefficients)')
+% 
+% % ------------------------------------------------------- B
+% figure(9);
+% surf(CWTcoeffs_B); 
+% colormap jet;
+% colorbar;
+% 
+% shading('interp'); view(-30,10);
+% xlabel('Sample (time)','fontsize',16);
+% ylabel('Scale level (Inverse of frequency)','fontsize',16)
+% zlabel('Intensity (coefficients)')
