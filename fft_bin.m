05880eabc20f17da1d7e18f54d50462608f6796d
ot316 <olithompson@rocketmail.com>
2020-04-22 16:56:38 +0100

    finish hypothesis testing

--- fft_bin.m
+++ fft_bin.m
@@ -1,9 +1,10 @@
 numberofbins = 500;
+numberoffiles = 40;
 f_bin_avg_close = zeros(604,numberofbins);
 f_bin_avg_far = zeros(1247,numberofbins);
 
 %% First Data set
-for i = 0:20
+for i = 0:numberoffiles
     [data,fs]=audioread(sprintf('AudioFiles 10.03.20/Nodule_taps/Nodule_tapstap%d.wav', i));                
     T = 1/fs;             % Sampling period       
     L = length(data);     % Length of signal
@@ -19,14 +20,14 @@ for i = 0:20
     oldii = 1;
 
     for ii = 1:binwidth:length(P1)
-        f_bin_avg_close(i+1,column) = mean(P1(oldii:ii));
+        f_bin_avg_close(i+1,column) = mean(P1(oldii:round(ii)));
         oldii = ii;
         column = column + 1;
     end      
 end
 
 %% second dataset
-for i = 0:20
+for i = 0:numberoffiles
     [data,fs]=audioread(sprintf('AudioFiles 10.03.20/No_Nodule_taps/No_Nodule_tapstap%d.wav', i));                
     T = 1/fs;         
     L = length(data);            
@@ -42,7 +43,7 @@ for i = 0:20
     oldii = 1;
 
     for ii = 1:binwidth:length(P1)
-        f_bin_avg_far(i+1,column) = mean(P1(oldii:ii));
+        f_bin_avg_far(i+1,column) = mean(P1(oldii:round(ii)));
         oldii = ii;
         column = column + 1;
     end   
@@ -56,7 +57,7 @@ ttest2matrix = zeros(numberofbins, 3);
 
 for i = 2:numberofbins+1
     [h,p] = ttest2(f_bin_avg_far(:,i-1), f_bin_avg_close(:,i-1), 'Alpha', 0.005);
-    ttest2matrix(i,1) = 22050/numberofbins*i;
+    ttest2matrix(i,1) = 22050/numberofbins*i-1;
     ttest2matrix(i,2) = h;
     ttest2matrix(i,3) = p;
 end
