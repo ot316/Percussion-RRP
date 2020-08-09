fa10e7620ae757d709922cf34ec16a35a88c29a1
Oli Thompson <olithompson@rocketmail.com>
2020-08-09 15:57:10 +0100

    get automatic scalogram generator workinh

--- MATLAB_Signal_Analysis/Automatic_Scalogram_Generator.m
+++ MATLAB_Signal_Analysis/Automatic_Scalogram_Generator.m
@@ -1,21 +1,19 @@
-clear all;
-input_filepath = "./Audio_Files_09.08.20_MATLAB/trimmed/";
-output_filepath = input_filepath + "scalograms/";
+close all;
+input_filepath = "../Audio_Files_09.08.20_MATLAB/trimmed/";
+output_filepath = "../Audio_Files_09.08.20_MATLAB/scalograms/";
 
 files=dir(input_filepath);
-for k=1:length(files)
-   path = input_filepath + files(k).name
-   [A,fs] = audioread(path)
-   whos A
-
+for k=3:length(files)
+    path = input_filepath + files(k).name;  %filepath
+    [A,fs] = audioread(path,[10,3000]);     %read sample
+    figure(1);
+    CWTcoeffs = cwt(A,5:5:1000,'sym10','plot');     %cwt for continuous 1D wavelet transform
+    colormap gray; 
+%     colorbar;
+    set(gca, 'Visible', 'off'); 
+    title('Matrix A - Close Nodule')
+    xlabel('Sample (time)','fontsize',16);
+    ylabel('Scale level (frequency)','fontsize',16)
+    saveas(gcf,(output_filepath + "/scalogram" + num2str(k) + ".png"))   %save figure to output filepath
 end
 
-% figure(1);
-% CWTcoeffs = cwt(A,5:5:500,'sym10','plot');     %cwt for continuous 1D wavelet transform
-% surf(CWTcoeffs)
-% colormap jet; 
-% colorbar;
-% 
-% title('Matrix A - Close Nodule')
-% xlabel('Sample (time)','fontsize',16);
-% ylabel('Scale level (Inverse of frequency)','fontsize',16)
