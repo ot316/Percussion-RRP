37f5cf1667a8c85f20b9a21e34b4b5394b8e1850
Oli Thompson <olithompson@rocketmail.com>
2020-10-07 20:44:59 +0100

    add readmes

--- Collected_Audio_Data/Find_Microphone_Device.py
+++
@@ -1,7 +0,0 @@
-// Author: Oli Thompson
-// This code prints the name of connected audio devices
-
-import pyaudio
-p = pyaudio.PyAudio()
-for ii in range(p.get_device_count()):
-    print(p.get_device_info_by_index(ii).get('name'))
