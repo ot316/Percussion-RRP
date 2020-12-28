5feba310ccac357be4b130e7760f54f47a948472
ot316 <olithompson@rocketmail.com>
2020-12-28 20:04:38 +0000

    modify scalogram generator to work on multiple dirs

--- Collected_Audio_Data/Experiment_02_Audio_Files_Original/Automatic_Scalogram_Generator.m
+++ Collected_Audio_Data/Experiment_02_Audio_Files_Original/Automatic_Scalogram_Generator.m
@@ -3,26 +3,30 @@
 % symlet 10 scalograms
 
 close all;
-input_filepath = "../Audio_Files_10.03.20_Experiment_Scalograms/Nodule_taps/";
-output_filepath = "../Audio_Files_10.03.20_Experiment_Scalograms/Nodule_taps/Scalograms/";
+files = dir;
+directoryNames = {files([files.isdir]).name};
+for i=3:length(directoryNames)
+    input_filepath = directoryNames(i) + "/";
+    output_filepath = directoryNames(i) + "/";
 
-files=dir(input_filepath);
-for k=3:length(files)-1 
-    path = input_filepath + files(k).name;  %filepath
-    [A,fs] = audioread(path,[4000,7000]);     %read sample
-    figure(1);
-    CWTcoeffs = cwt(A,5:5:500,'sym10','plot'); 
-    %cwt for continuous 1D wavelet transform
-    colormap gray; 
-    colorbar;
-    title('Far Nodule')
-    xlabel('Sample (time)','fontsize',16);
-    ylabel('Frequency','fontsize',16)
-    
-    %uncommenting the below lines will save the plots and overwrite the
-    %existing scalograms, use carefully
+    files=dir(input_filepath);
+    for k=3:length(files)-1 
+        path = input_filepath + files(k).name;  %filepath
+        [A,fs] = audioread(path,[3000,8000]);     %read sample
+        figure(1);
+        CWTcoeffs = cwt(A,5:5:1000,'sym10','plot'); 
+        %cwt for continuous 1D wavelet transform
+        colormap gray; 
+    %     colorbar; 
+        title('Far Nodule')
+        xlabel('Sample (time)','fontsize',16);
+        ylabel('Frequency','fontsize',16)
 
-%     set(gca, 'Visible', 'off'); 
-%     saveas(gcf,(output_filepath + num2str(k-3) + ".png"))   %save figure to output filepath
-%     close
+        %uncommenting the below lines will save the plots and overwrite the
+        %existing scalograms, use carefully
+        output_path = (output_filepath + regexprep(output_filepath,'[/]','') + "_scalogram_" + num2str(k-3) + ".png")
+        set(gca, 'Visible', 'off'); 
+        saveas(gcf,output_path)   %save figure to output filepath
+        close
+    end
 end
