436434d28749b953ddfc568ed23c29c7c3f7465d
ot316 <olithompson@rocketmail.com>
2020-12-30 14:39:08 +0000

    transfer images and alter process training data script

--- Collected_Audio_Data/Experiment_02_Audio_Files_Original/Automatic_Scalogram_Generator.m
+++ Collected_Audio_Data/Experiment_02_Audio_Files_Original/Automatic_Scalogram_Generator.m
@@ -5,26 +5,30 @@
 close all;
 files = dir;
 directoryNames = {files([files.isdir]).name};
-for i=3:length(directoryNames)
+for i=9:length(directoryNames)
     input_filepath = directoryNames(i) + "/";
-    output_filepath = directoryNames(i) + "/";
 
     files=dir(input_filepath);
     for k=3:length(files)-1 
         path = input_filepath + files(k).name;  %filepath
-        [A,fs] = audioread(path,[3000,8000]);     %read sample
+        try
+          [A,fs] = audioread(path,[3000, 9000]);     %read sample
+        catch
+            fprintf("error with " + path + '\n');
+            continue
+        end
         figure(1);
         CWTcoeffs = cwt(A,5:5:1000,'sym10','plot'); 
-        %cwt for continuous 1D wavelet transform
+%           cwt for continuous 1D wavelet transform
         colormap gray; 
-    %     colorbar; 
-        title('Far Nodule')
-        xlabel('Sample (time)','fontsize',16);
-        ylabel('Frequency','fontsize',16)
+%         colorbar; 
+%         title('Far Nodule')
+%         xlabel('Sample (time)','fontsize',16);
+%         ylabel('Frequency','fontsize',16)
 
         %uncommenting the below lines will save the plots and overwrite the
         %existing scalograms, use carefully
-        output_path = (output_filepath + regexprep(output_filepath,'[/]','') + "_scalogram_" + num2str(k-3) + ".png")
+        output_path = (input_filepath + regexprep(input_filepath,'[/]','') + "_scalogram_" + num2str(k-3) + ".png")
         set(gca, 'Visible', 'off'); 
         saveas(gcf,output_path)   %save figure to output filepath
         close
